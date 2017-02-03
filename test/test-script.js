d86e728f35d70e01faff163d207d4832971966b7
nms377 <nms377@nyu.edu>
2017-02-02 19:13:33 -1000

    My tests aren't strict. My should be a number test still passes if I swap an array of number with an array of strings.

--- test/test-script.js
+++ test/test-script.js
@@ -4,4 +4,29 @@ const chai = require('chai');
 
 chai.should();
 
-const bubbleSort = require('../bubble-sort.js');
+const BubbleSort = require('../bubble-sort.js');
+
+describe('myArray', () => {
+
+	let myArray = BubbleSort;
+
+	it('should be an array', () => {
+		myArray.should.be.an.array;
+	});
+
+	it('should be a number', () => {
+		myArray.should.be.a.number;
+	});
+
+});
+
+describe('sortMyArray', () => {
+
+	let bubbleSort = BubbleSort;
+
+	it('should be a function', () => {
+		bubbleSort.should.be.a('function');
+	});
+
+});
+
