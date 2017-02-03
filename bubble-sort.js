baaf8b07b5f106e720a577764e2d968233bf6f31
nms377 <nms377@nyu.edu>
2017-02-02 21:05:55 -1000

    Console logged the function to figure out where my test was bugging out.

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,26 +1,22 @@
-let arr = [5,2,9,1,3,6,10,8,7,4];
+// let arr = [5,2,9,1,3,6,10,8,7,4];
 
-console.log('outside', arr);
+// console.log('outside', arr);
 
-(function bubbleSort (array){
+function bubbleSort (n){
 
-var n = array;
-
-console.log(n);
-swapped = false;
 	do{
-		for( i=1; i<n.length; i++ ){
+		swapped = false;
+		for( i=1; i < n.length; i++ ){
 			if( n[i-1] > n[i] ){
 				tmp = n[i];
 				n[i] = n[i-1];
 				n[i-1] = tmp;
 				swapped = true;
-				console.log(n);
 			}
 		}
 	}
 	while(swapped);
 	return n;
-})();
+}
 
-module.export =bubbleSort;
+module.exports = bubbleSort;
