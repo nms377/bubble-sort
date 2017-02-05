70ecd9f61d3541af3bfa260d8f6106da92d39f3c
nms377 <nms377@nyu.edu>
2017-02-04 20:40:58 -1000

    Made my tests for merge. I don't think some of it works.

---
+++ test/test-merge-sort.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+
+chai.should();
+
+const mergeSort = require('../merge-sort');
+
+describe('mergeSort', () => {
+
+	it('should be a function', () => {
+		mergeSort.should.be.a('function');
+	});
+
+	it('should not return a string', () =>{
+		mergeSort.should.not.be.a('string');
+	});
+
+	it('should return an array', () => {
+		mergeSort([]).should.be.deep.equal([]);
+	});
+
+	it('array should not be empty', () => {
+		mergeSort.should.not.be.empty;
+	});
+
+	it('should not equal null', () => {
+		mergeSort.should.not.equal('null');
+	});
+
+});
